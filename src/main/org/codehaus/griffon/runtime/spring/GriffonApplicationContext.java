8cf3475273ed8f0ec4ff6bc3e891f708c58adf9d
aalmiray <aalmiray@gmail.com>
2013-01-10 22:42:35 +0100

    Update copyright notice

--- src/main/org/codehaus/griffon/runtime/spring/GriffonApplicationContext.java
+++ src/main/org/codehaus/griffon/runtime/spring/GriffonApplicationContext.java
@@ -1,5 +1,5 @@
 /* 
- * Copyright 2004-2012 Graeme Rocher
+ * Copyright 2004-2013 Graeme Rocher
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
